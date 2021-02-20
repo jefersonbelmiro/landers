dadc407c0b73f10da498035b57cceb9923fb5224
Jeferson Belmiro <jeferson.belmiro@gmail.com>
2021-02-20 12:28:42 -0300

    fix gh-pages script

--- ghpages.sh
+++ ghpages.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-git checkout gh-pages && cp * build/webgl/* -rvp . && git push 
+git checkout gh-pages && \
+    cp * build/webgl/* -rvf . ; \
+    git add Build* TemplateData* index.html && \
+    git commit -m 'update' && \
+    git push && \
+    git checkout -
